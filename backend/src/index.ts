443ccbe61c0af4ad1db198bf45c2385def0b56d8
titanpascal41 <66498085+titanpascal41@users.noreply.github.com>
2026-01-13 09:24:30 +0000

    derniere mise

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ import cadreTemporelRoutes from "./routes/cadreTemporel";
 config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
